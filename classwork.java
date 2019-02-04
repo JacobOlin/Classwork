c3427e94dbf5e3b1cf794b023ab79726fe180421
Jacob Olin <jolin00@stuy.edu>
2019-02-04 11:37:14 -0500

    Wrote the first version of the two functions

--- classwork.java
+++ classwork.java
@@ -4,11 +4,19 @@ public class classwork{
 
   public static List<String> makeAllWords(int k, int maxLetter) {
     List<String> l = new ArrayList<String>();
+    mAW(k,"",l,maxLetter);
     return l;
   }
 
-  public static void mAW(int k, String word, List l) {
-
+  public static void mAW(int k, String word, List<String> l, int maxLetter) {
+    if (k == 0) {
+      l.add(word);
+    }
+    else {
+      for (int i = 0;i < maxLetter;i += 1) {
+        mAW(k-1,word + ((char)('a' + i)),l,maxLetter);
+      }
+    }
   }
 
 }
