458c8e014276af596805efba995765675b2cf0d1
Jacob Olin <jolin00@stuy.edu>
2019-02-04 11:33:21 -0500

    Wrote the shells for makeAllWords and the helper function

---
+++ classwork.java
@@ -0,0 +1,14 @@
+import java.util.List;
+import java.util.ArrayList;
+public class classwork{
+
+  public static List<String> makeAllWords(int k, int maxLetter) {
+    List<String> l = new ArrayList<String>();
+    return l;
+  }
+
+  public static void mAW(int k, String word, List l) {
+
+  }
+
+}
