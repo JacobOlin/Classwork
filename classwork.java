efd6b70b857e44b28f3488e97555665917477a3e
JacobOlin <jolin00@stuy.edu>
2019-03-10 22:01:03 -0400

    Wrote partition

--- classwork.java
+++ classwork.java
@@ -1,5 +1,6 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Random;
 public class classwork{
 
   public static void main(String[] args) {
@@ -27,6 +28,37 @@ public class classwork{
         mAW(k-1,word + ((char)('a' + i)),l,maxLetter);
       }
     }
+  }
+
+  public static int partition(int[] data,int start, int end) {
+    Random r  = new Random();
+    int index = start + r.nextInt()%(end - start + 1);
+    int pivot = data[index];
+    data[index] = data[start];
+    data[start] = pivot;
+    return recursive(data,start+1,end,start);
+  }
+
+  public static int recursive(int[] data,int start,int end,int pivotInd) {
+    if (start == end) {
+      if (data[pivotInd] > data[start]) {
+        int pivot = data[pivotInd];
+        data[pivotInd] = data[start - 1];
+        data[start - 1] = pivot;
+        return start - 1;
+      }
+      int pivot = data[pivotInd];
+      data[pivotInd] = data[start];
+      data[start] = pivot;
+      return start;
+    }
+    if (data[start] >= data[pivotInd]) {
+      int change = data[start];
+      data[start] = data[end];
+      data[end] = change;
+      return recursive(data,start,end - 1,pivotInd);
+    }
+    return recursive(data,start+1,end,pivotInd)
   }
 
 }
