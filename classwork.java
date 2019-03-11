ea1dabcb5278d3429690c8386b74cecdd3fecd94
JacobOlin <jolin00@stuy.edu>
2019-03-10 22:18:09 -0400

    Fixed a couple of issues with partition but it still doesn't completely work

--- classwork.java
+++ classwork.java
@@ -31,17 +31,25 @@ public class classwork{
   }
 
   public static int partition(int[] data,int start, int end) {
+    if (start == end) {
+      return end;
+    }
     Random r  = new Random();
     int index = start + r.nextInt()%(end - start + 1);
     int pivot = data[index];
     data[index] = data[start];
     data[start] = pivot;
+    /*System.out.print("ORIG: ");
+    for (int i = 0;i < data.length;i += 1) {
+      System.out.print(data[i] + " ");
+    }
+    System.out.println("Pivot "+ pivot);*/
     return recursive(data,start+1,end,start);
   }
 
   public static int recursive(int[] data,int start,int end,int pivotInd) {
     if (start == end) {
-      if (data[pivotInd] > data[start]) {
+      if (data[pivotInd] < data[start]) {
         int pivot = data[pivotInd];
         data[pivotInd] = data[start - 1];
         data[start - 1] = pivot;
@@ -58,7 +66,7 @@ public class classwork{
       data[end] = change;
       return recursive(data,start,end - 1,pivotInd);
     }
-    return recursive(data,start+1,end,pivotInd)
+    return recursive(data,start+1,end,pivotInd);
   }
 
 }
